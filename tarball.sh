8e92d58e3e79aebabe26f8425bb19e9ab8b7dcbe
Glenn Brown <glenn@myri.com>
2010-09-09 21:20:51 -0700

    [lanai] Refine tarball.sh more

--- tarball.sh
+++ tarball.sh
@@ -10,25 +10,32 @@ die () {
 must () {
     "$@" || die "$@" "failed."
 }
+
+# V is the release version number, extracted from the source code.
 V=`fgrep AM_INIT_AUTOMAKE bfd/configure.in \
     | sed -e 's/[\t ]*//g' \
     | sed -e 's@[^,]*,\([^)]*\).*@\1@' \
     | sed -e 's/[^-._a-zA-Z0-9]/_/g'` || die "Could not extract version"
+# NM is the name of untarred directory, and basename of tarball.
 NM="lanai-binutils-$V"
 tarball="../$NM.tar.bz2"
 
-# Verify the build is likely to work.
+# Verify that all files that will be tarred are committed, so we have a
+# record of exactly what was released.
 
+# First, verify that there are no untracked files or directories.
 test `git clean -nfxd | wc -l` = 0 || die "Need to \"git clean -fxd\" with care"
+# Next, verify all changes are committed.
 test `git status | fgrep modified: | wc -l` = 0 || die "Uncommitted modifications"
 
 # Verify the user wants to build the tarball.
+# This is a subtle reminder that the version number might need updating.
 
 echo -n "Build $tarball? "
 read yes
 test "x$yes" = "xyes" || die "You did not type \"yes\"."
 
-# Verify builds work, and cleanup.
+# Verify builds work, and cleanup.  This is not required.
 
 must ./configure CFLAGS="-O2 -g" --target=lanai --enable-shared --prefix="`pwd`/install"
 must make -j8
